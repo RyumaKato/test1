824bb951d28d984dec03547fb54d37549b7277da
RyumaKato <karuwa.adw1@gmail.com>
2023-08-15 14:48:59 +0900

    9-2-3 カテゴリーごと

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,5 +11,10 @@ class Category extends Model
 {
     return $this->hasMany(Post::class);  
 }
-    use HasFactory;
+use HasFactory;
+
+public function getByCategory(int $limit_count = 5)
+{
+     return $this->posts()->with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
+}
 }
