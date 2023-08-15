0b8102035a2e7f23005f325c05ea77d38c58b004
RyumaKato <karuwa.adw1@gmail.com>
2023-08-15 16:12:27 +0900

    9-3 認証機能

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,9 +1,8 @@
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
-    </head>
-    <body>
+<x-app-layout>
+    <x-slot name="header">
+        　（INDEX）
+    </x-slot>
+
         <h1>Blog Name</h1>
         <form action="/posts" method="POST">
             @csrf
@@ -29,5 +28,4 @@
         <div class="back">
             <a href="/">back</a>
         </div>
-    </body>
-</html>
+      </x-app-layout>
